7e55686fdfaea2e51af7989a16bb74d6cccd6d1e
beausimpson <simpson.beau@gmail.com>
2019-02-17 23:12:07 -0600

    Updates

---
+++ precache-manifest.2050d31ea228c14c3acfce606c1aad2b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c14ddbf9f748c87ae5a",
+    "url": "/beausimpson/clicky-game/static/js/runtime~main.8c14ddbf.js"
+  },
+  {
+    "revision": "b6d5e9ac17cdf932af4a",
+    "url": "/beausimpson/clicky-game/static/js/main.b6d5e9ac.chunk.js"
+  },
+  {
+    "revision": "6538b0b2001755344512",
+    "url": "/beausimpson/clicky-game/static/js/2.6538b0b2.chunk.js"
+  },
+  {
+    "revision": "b6d5e9ac17cdf932af4a",
+    "url": "/beausimpson/clicky-game/static/css/main.d0a19a01.chunk.css"
+  },
+  {
+    "revision": "dbfe9b0acdc3bd9e01db34757d7223bc",
+    "url": "/beausimpson/clicky-game/index.html"
+  }
+];
